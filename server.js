c437efa546b9e5383d49f2e10b73e379312d7137
en-chang <enchangcs@gmail.com>
2021-06-13 22:50:14 -0500

    Connect /signin and /register to database

--- server.js
+++ server.js
@@ -20,72 +20,61 @@ app.use(express.urlencoded({
 }));
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '1234',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 4,
-      joined: new Date()
-    },
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-  // Load hash from your password DB.
-  bcrypt.compare("apples", "$2a$10$/8hzsGhmsPpxKdM5nbk6yORPYeWfYJYfCdkkDt7itF9K1/Fua72Mm", function(err, res) {
-    console.log('first guess', res);
-  });
-  bcrypt.compare("!apples", "$2a$10$/8hzsGhmsPpxKdM5nbk6yORPYeWfYJYfCdkkDt7itF9K1/Fua72Mm", function(err, res) {
-    console.log('second guess', res);
-  });
-  if (req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      // Used to check encrypted password
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(password, salt, function(err, hash) {
-        console.log(hash);
-    });
-  });
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
+  if (password === undefined) {
+    res.status(400).json('undefined password');
+  }
+  // Used to encrypt password
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  db.transaction(trx => {
+    trx.insert({
       email: email,
-      joined: new Date()
+      hash: hash
     })
-    .then(user => {
-      res.json(user[0]);
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          name: name,
+          email: loginEmail[0],
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
     })
+    .then(trx.commit)
+    .then(trx.rollback)
     .catch(err => res.status(400).json('unable to register'))
+  })
 })
 
 app.get('/profile/:id', (req, res) => {
