cb71ac437d6dcf726988224c0737b0bdaa8a7c9a
en-chang <enchangcs@gmail.com>
2021-07-08 04:34:39 -0500

    Add morgan for logging and get docker-compose file to work properly

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const profile = require('./controllers/profile');
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(morgan('combined'))
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('Server is running') })
@@ -38,4 +40,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => {})
+// app.listen(process.env.PORT || 3000, () => {})
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
