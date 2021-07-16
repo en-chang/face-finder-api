264c7883c460a861a9051cee2fdc71b394f8f24b
en-chang <enchangcs@gmail.com>
2021-07-15 23:52:15 -0500

    Test commit and remove console log

--- server.js
+++ server.js
@@ -36,6 +36,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 // app.listen(process.env.PORT || 3000, () => {})
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.listen(3000, () => {})
