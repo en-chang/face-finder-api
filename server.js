e75e3fdb6662f7aa8e7de6154a00d1fa549320b2
en-chang <enchangcs@gmail.com>
2021-07-25 22:22:12 -0500

    Add authorization to major end points

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 const morgan = require('morgan');
+const auth = require('./controllers/authorization');
+
 const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const { requireAuth } = require('./controllers/authorization');
 
 const db = knex({
   // client: 'pg',
@@ -31,10 +34,10 @@ app.use(cors());
 app.get('/', (req, res) => { res.send('Server is running') })
 app.post('/signin', (req, res) => { signin.signinAuthentication(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res) })
 
 // app.listen(process.env.PORT || 3000, () => {})
 app.listen(3000, () => {})
